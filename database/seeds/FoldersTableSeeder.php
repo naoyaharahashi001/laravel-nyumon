cc2cde74e1282cb593e46e0b1d2f35f7266a6411
miyabi <mogamimaly3603@gmail.com>
2020-04-28 17:58:58 +0900

    chapter.3 commit

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FoldersTableSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class FoldersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $titles = ['プライベート', '仕事', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
