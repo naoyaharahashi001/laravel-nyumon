b238d931ba4fb95c4f62e9c964c0fac7faae9ace
miyabi <mogamimaly3603@gmail.com>
2020-05-13 17:35:57 +0900

    チュートリアル４　Task モデルにアクセサを追加する

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
-//use App\Task; // ★　追加
+use App\Task; // ★　追加
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,16 +13,16 @@ class TaskController extends Controller
         // すべてのフォルダを取得する
         $folders = Folder::all();
 
-        //　選ばれたフォルダを取得する
-//        $current_folder = Folder::find($id);
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
 
         // 選ばれたフォルダに紐づくタスクを取得する
-//        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-//            'current_folder_id' => $id,
-//            'tasks' => $tasks,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
