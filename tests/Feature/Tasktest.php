0a7f2b0a8b15a67f03ff5e92a7510fe8776c8fa2
miyabi <mogamimaly3603@gmail.com>
2020-05-22 10:49:50 +0900

    チュートリアル6　フォーム表示のためにTaskController.php に showEditForm を追加

--- tests/Feature/Tasktest.php
+++ tests/Feature/Tasktest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Http\Requests\CreateTask;
+use Carbon\Carbon;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class TaskTest extends TestCase
 {
