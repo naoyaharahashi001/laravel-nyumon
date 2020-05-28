b514d070b9d36e31a1917c34c4a579853338f755
miyabi <mogamimaly3603@gmail.com>
2020-05-28 15:23:46 +0900

    反映されていなかったタスクの状態「未着手」「着手中」「完了」を表示するようにする

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -66,16 +66,16 @@ class TaskController extends Controller
 
     public function edit(int $id, int $task_id, EditTask $request)
     {
-        // 1
+        // 1リエストされた ID でタスクデータを取得
         $task = Task::find($task_id);
 
-        // 2
+        // 2編集対象のタスクデータに入力値を詰めて save
         $task->title = $request->title;
         $task->status = $request->status;
         $task->due_date = $request->due_date;
         $task->save();
 
-        // 3
+        // 3編集対象のタスクが属するタスク一覧画面へリダイレクト
         return redirect()->route('tasks.index', [
             'id' => $task->folder_id,
         ]);
